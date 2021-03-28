e6884113ce49fb66642546a7dec4d1598451f3d4
ebtesam25 <ebtesam@iut-dhaka.edu>
2021-03-28 07:28:45 +0300

    splash

--- rnapp/app/src/screens/welcome.js
+++ rnapp/app/src/screens/welcome.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Image, Button, ImageBackground } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, ImageBackground, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import Svg, { Line, Circle } from 'react-native-svg';
 import { Icon } from 'react-native-elements'
@@ -12,15 +12,15 @@ import { useFonts } from 'expo-font';
 export default function Welcome() {
     const navigation = useNavigation();
     const [fontLoaded] = useFonts({
-        BR: require('../assets/fonts/BR.otf'),
-        AR: require('../assets/fonts/AR.otf'),
+        G: require('../assets/fonts/G.ttf'),
+        K: require('../assets/fonts/K.ttf'),
 
       });
-      useEffect(() => {
-        setTimeout(() => {
-            navigation.navigate('Join');
-        }, 1000);
-      },[]);
+      // useEffect(() => {
+      //   setTimeout(() => {
+      //       navigation.navigate('#');
+      //   }, 1000);
+      // },[]);
 
       if (!fontLoaded) {
         return null;
@@ -30,11 +30,17 @@ export default function Welcome() {
    
     return (
         <View style={styles.container}>
-            <View style={{ marginTop: '25%', alignSelf:'center' }}>
-              <Image source={require('../assets/splash.png')}></Image>
-              <View style={{position:'absolute', zIndex:2, top:'50%', alignSelf:'center'}}>
-                <Text style={{fontFamily:'BR', fontSize:90, color:'#FF5C00'}}>Safe<Text style={{color:'#0085FF'}}>Walk</Text></Text>
-                <Text style={{fontFamily:'AR', fontSize:25, color:'#000', textAlign:'center'}}>Stay Safe, Walk Freely</Text>
+            <View style={{ marginTop: '55%',}}>
+              <Image source={require('../assets/logo.png')} style={styles.header} ></Image>
+              <View style={{position:'relative', marginLeft:'10%', marginTop:'5%'}}>
+                <Text style={{fontFamily:'G', fontSize:40, color:'#F65158'}}>OurFund</Text>
+                <Text style={{fontFamily:'K', fontSize:25, color:'#000'}}>Democratized Lending Circles</Text>
+              </View>
+              <View style={{backgroundColor:'#F65158', marginTop:'15%', width:'60%', alignSelf:'center', paddingVertical:'5%', borderRadius:10}}>
+                <TouchableOpacity><Text style={{textAlign:'center', color:'#FBDE44', fontFamily:'K', fontSize:18}}>Sign In</Text></TouchableOpacity>
+              </View>
+              <View style={{backgroundColor:'#FFBF00', marginTop:'2.5%', width:'60%', alignSelf:'center', paddingVertical:'5%', borderRadius:10}}>
+              <TouchableOpacity><Text style={{textAlign:'center', color:'#28324B', fontFamily:'K', fontSize:18}}>Create an Account</Text></TouchableOpacity>
               </View>
             </View>
         </View>
@@ -49,11 +55,10 @@ const styles = StyleSheet.create({
         backgroundColor: '#FFF'
     },
     header: {
-        height: '55%',
-        width: '100%',
-        marginTop: '-5%',
-        resizeMode: 'contain',
-        alignSelf: 'center'
+        height:'34%',
+        width:'34%',
+        resizeMode:'contain',
+        marginLeft:'10%'
     },
 
 });
